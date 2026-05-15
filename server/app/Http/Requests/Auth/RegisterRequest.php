adc63bae1bbe0636035db46ec133e4d60732d7d4
Matheus <mps.web.tech@gmail.com>
2026-05-15 16:33:29 -0300

    feat: Avoiding bot attacks

--- server/app/Http/Requests/Auth/RegisterRequest.php
+++ server/app/Http/Requests/Auth/RegisterRequest.php
@@ -6,19 +6,17 @@ use Illuminate\Foundation\Http\FormRequest;
 
 class RegisterRequest extends FormRequest
 {
-    /**
-     * Determine if the user is authorized to make this request.
-     */
+
     public function authorize(): bool
     {
-        return true;
+        if ($this->filled('username_field_check')) {
+            \Log::warning("Bot detectado via Honeypot. IP: " . $this->ip());
+            return false; 
+        }
+
+        return true; 
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
     public function rules(): array
     {
         return [
@@ -32,19 +30,19 @@ class RegisterRequest extends FormRequest
     {
         return [
             'name.required' => 'O nome é obrigatório.',
-            'name.string'   => 'O nome deve ser um texto válido.',
-            'name.max'      => 'O nome não pode ter mais que :max caracteres.',
+            'name.string' => 'O nome deve ser um texto válido.',
+            'name.max' => 'O nome não pode ter mais que :max caracteres.',
 
             'email.required' => 'O e-mail é obrigatório.',
-            'email.string'   => 'O e-mail deve ser um texto válido.',
-            'email.email'    => 'Informe um endereço de e-mail válido.',
-            'email.max'      => 'O e-mail não pode ter mais que :max caracteres.',
-            'email.unique'   => 'Este e-mail já está em uso.',
+            'email.string' => 'O e-mail deve ser um texto válido.',
+            'email.email' => 'Informe um endereço de e-mail válido.',
+            'email.max' => 'O e-mail não pode ter mais que :max caracteres.',
+            'email.unique' => 'Este e-mail já está em uso.',
 
             'password.required' => 'A senha é obrigatória.',
-            'password.string'   => 'A senha deve ser um texto válido.',
-            'password.min'      => 'A senha deve ter no mínimo :min caracteres.',
-            'password.max'      => 'A senha não pode ter mais que :max caracteres.',
+            'password.string' => 'A senha deve ser um texto válido.',
+            'password.min' => 'A senha deve ter no mínimo :min caracteres.',
+            'password.max' => 'A senha não pode ter mais que :max caracteres.',
         ];
     }
 }
